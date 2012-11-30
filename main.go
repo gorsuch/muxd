13daf645faeb4f0496a85dd4f617e78119e2de91
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-11-30 14:31:39 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+import "net/http"
+import "os"
+import "time"
+import "github.com/fzzbt/radix/redis"
+
+func main() {
+	conf := redis.DefaultConfig()
+	c := redis.NewClient(conf)
+	defer c.Close()
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			c.Publish("mux", r.FormValue("data"))
+		}
+
+		if r.Method == "GET" {
+			for {
+				fmt.Fprintf(w, "%s\n", r.Method)
+				if f, ok := w.(http.Flusher); ok {
+					f.Flush()
+				}
+				time.Sleep(time.Second)
+			}
+		}
+	}
+
+	http.HandleFunc("/", h)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
